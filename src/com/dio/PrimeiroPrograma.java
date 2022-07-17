c67afd87cbb8eac8117701c1acc830b8ff35b962
day <daynaramira@gmail.com>
2022-07-17 15:52:19 -0300

    Criando a classe Livro

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -9,7 +9,43 @@ public class PrimeiroPrograma {
         System.out.println("Hello World! " + (a+b));*/
 
         Gato gato = new Gato();
-
         System.out.println(gato);
+
+        Livro livro1 = new Livro("O morro dos Ventos Uivantes", 368);
+        System.out.println(livro1);
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer npag;
+
+    public Livro(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npag=" + npag +
+                '}';
     }
 }
